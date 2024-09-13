0661dcd04748bd6073841393e7352a84b20093f1
maundu55 <marymaundu.mm@gmail.com>
2024-09-14 02:38:27 +0300

    Color Changee project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Square from "./Square"
+import Input from "./Input"
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <Square />
+      <Input />
+    </div>
+  )
+}
+
+export default App
