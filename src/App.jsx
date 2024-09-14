ff7b2abf2f896fd03ea3d624072144a5c1094e3f
maundu55 <marymaundu.mm@gmail.com>
2024-09-14 09:23:23 +0300

    add hex values

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,18 @@ import Input from "./Input"
 import { useState } from "react"
 
 function App() {
-const [colorValue, setColorValue] = useState('')
+const [colorValue, setColorValue] = useState('');
+const [hexValue, setHexValue] = useState('')
+const [isDarkText, setIsDarkText] = useState(true)
  
   return (
     <div className="App">
-      <Square colorValue={colorValue} />
+      <Square colorValue={colorValue}
+              hexValue={hexValue}
+      />
       <Input colorValue={colorValue}
-             setColorValue={setColorValue}     
+             setColorValue={setColorValue}
+             setHexValue ={setHexValue}     
       />
     </div>
   )
