1e6f28b3a7452c55e31492e512f26a05a1b11af5
maundu55 <marymaundu.mm@gmail.com>
2024-09-14 03:11:31 +0300

    add input, square components and css styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Square from "./Square"
 import Input from "./Input"
+import { useState } from "react"
 
 function App() {
+const [colorValue, setColorValue] = useState('')
  
   return (
     <div className="App">
-      <Square />
-      <Input />
+      <Square colorValue={colorValue} />
+      <Input colorValue={colorValue}
+             setColorValue={setColorValue}
+             onChange={onchange}
+      />
     </div>
   )
 }
